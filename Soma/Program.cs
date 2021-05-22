91423700f5319c121a534f57c9bbd2f47ebf4030
MariiMariis <78056802+MariiMariis@users.noreply.github.com>
2021-05-21 23:14:37 -0300

    Adicionar arquivos de projeto.

---
+++ Soma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Soma
+{
+    class ProgramaSoma
+    {
+        static void Main()
+
+        {
+            int numero1;
+            int numero2;
+            int soma;
+
+            
+            Console.WriteLine("Digite o primeiro número: ");
+            var primeiroNumero = Console.ReadLine();
+
+            Console.WriteLine("Digite o segundo número: ");
+            var segundoNumero = Console.ReadLine();
+
+            numero1 = Int32.Parse(primeiroNumero);
+            numero2 = Int32.Parse(segundoNumero);
+
+
+            soma = numero1 + numero2;
+
+            Console.WriteLine($"O resultado da soma dos números {primeiroNumero} e {segundoNumero} é + {soma}!");
+
+        }
+    }
+}
---
+++ Subtração/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Subtração
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
