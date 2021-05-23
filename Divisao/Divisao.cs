1c630478a5549eba4884043de2ab62dbe54ad558
MariiMariis <78056802+MariiMariis@users.noreply.github.com>
2021-05-22 22:00:54 -0300

    Alteração de nome e inclusão de divisão e multiplicação.

---
+++ Divisao/Divisao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Divisao
+{
+    class ProgramaDivisao
+    {
+        static void Main()
+
+        {
+            int numero1;
+            int numero2;
+            int divisao;
+
+            Console.WriteLine("Digite o primeiro número: ");
+            var primeiroNumero = Console.ReadLine();
+
+            Console.WriteLine("Digite o segundo número: ");
+            var segundoNumero = Console.ReadLine();
+
+            numero1 = Int32.Parse(primeiroNumero);
+            numero2 = Int32.Parse(segundoNumero);
+
+            divisao = numero1 / numero2;
+
+            Console.WriteLine($"O resultado da divisão dos números {primeiroNumero} e {segundoNumero} é {divisao}!");
+
+        }
+    }
+}
